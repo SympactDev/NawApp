5fe4c0bcd79a8ebfe01110be995b55e9b623b994
roland flentge <roland.flentge@gmail.com>
2021-05-09 13:43:05 +0200

    cddsd

--- NawUI/Program.cs
+++ NawUI/Program.cs
@@ -4,7 +4,9 @@ namespace NawUI
 {
     class Program
     {
+#pragma warning disable IDE0060 // Remove unused parameter
         static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
         {
             Console.WriteLine("Hello World!");
         }
